472e0af0d50fad9d64ebb750d04d65e29d4fe491
mgmix <evace93@gmail.com>
2021-04-18 22:48:43 +0900

    create project

---
+++ app/src/main/java/mgmix/dev/collection_of_android_project/MainActivity.kt
@@ -0,0 +1,11 @@
+package mgmix.dev.collection_of_android_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
